83cec61c70a282b928b63aa9ec691280b3ff565b
Gabriel França <dev.gabrielfranca@gmail.com>
2020-08-18 22:24:35 -0300

    adding styled-components

---
+++ pages/home/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const Home: React.FC = () => {
+  return <S.Title>Hello World!</S.Title>;
+};
+
+export default Home;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,1 @@
-import React from 'react';
-
-const Home: React.FC = () => {
-  return <h1>Hello World</h1>;
-};
-
-export default Home;
+export { default } from './home';
